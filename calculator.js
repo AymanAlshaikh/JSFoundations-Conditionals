2320906d77d39344e48de9cefeba41eeadd839f6
AymanAlshaikh <ayman159@live.com>
2021-01-10 16:34:41 +0300

    dONe

--- calculator.js
+++ calculator.js
@@ -1,18 +1,21 @@
 // Define the numbers and the operation here
 const numOne = 10;
 const numTwo = 7;
-let operator = "*";
+let operator = "-";
 // Write if-statements here
 if (operator === "+"){
     console.log("addition")
+    console.log(numOne + numTwo)
 }
 else if (operator === "-"){
     console.log("subtraction")
+    console.log(numOne - numTwo)
 }
 else if (operator === "*"){
     console.log("Multiplication")
+    console.log(numOne * numTwo)
 }
 else{
     console.log("Division")
+    console.log(numOne / numTwo)
 }
-console.log(`${numOne + numTwo}`)
